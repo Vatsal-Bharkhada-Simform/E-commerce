0547e520afd158c8fe5ba1e841eb1c0b4aee796b
Vatsal Bharkhada <vatsal.bharkhada@simformsolutions.com>
2026-06-11 10:40:28 +0530

    Feat: Add zod validators to validate products API response

---
+++ src/validators/ProductValidator.ts
@@ -0,0 +1,22 @@
+import z from "zod";
+
+const CategoryValidator = z.object({
+	id: z.number(),
+	name: z.string(),
+	image: z.string(),
+	slug: z.string(),
+});
+
+const ProductValidator = z.object({
+	id: z.number(),
+	title: z.string(),
+	slug: z.string(),
+	price: z.number(),
+	description: z.string(),
+	category: CategoryValidator,
+	images: z.array(z.string()),
+});
+
+const ProductListValidator = z.array(ProductValidator);
+
+export { ProductValidator, ProductListValidator };
