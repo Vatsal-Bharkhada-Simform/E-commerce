3e19a0300306615ec650911da064d5cbc7fb0e9c
Vatsal Bharkhada <vatsal.bharkhada@simformsolutions.com>
2026-06-12 18:29:40 +0530

    Refactor: Convert list container and list item to compound components

--- src/components/NavList.tsx
+++ src/components/NavList.tsx
@@ -1,49 +1,14 @@
-import {
-	Heart,
-	HomeSmile,
-	Package,
-	ShoppingBag01,
-	UserCircle,
-} from "@untitledui/icons";
-import type { ListItemType } from "../types/ComponentTypes";
+import type { ReactNode } from "react";
 import { ListItem } from "./ListItem";
 
-const navListContent: Array<ListItemType> = [
-	{
-		title: "Home",
-		icon: <HomeSmile className="w-5" />,
-		isActive: true,
-	},
-	{
-		title: "Products",
-		icon: <Package className="w-5" />,
-		isActive: false,
-	},
-	{
-		title: "Wishlist",
-		icon: <Heart className="w-5" />,
-		isActive: false,
-	},
-	{
-		title: "Orders",
-		icon: <ShoppingBag01 className="w-5" />,
-		isActive: false,
-	},
-	{
-		title: "Account",
-		icon: <UserCircle className="w-5" />,
-		isActive: false,
-	},
-];
-
-export function NavList() {
+function NavList({ children }: { children: ReactNode }) {
 	return (
 		<nav>
-			<ul className="list-none flex flex-col flex-1 gap-2">
-				{navListContent.map((item) => (
-					<ListItem {...item} key={item.title} />
-				))}
-			</ul>
+			<ul className="list-none flex flex-col flex-1 gap-2">{children}</ul>
 		</nav>
 	);
 }
+
+NavList.ListItem = ListItem;
+
+export { NavList };
--- src/layout/Sidebar.tsx
+++ src/layout/Sidebar.tsx
@@ -1,16 +1,53 @@
+import {
+	Heart,
+	HomeSmile,
+	Package,
+	ShoppingBag01,
+	UserCircle,
+} from "@untitledui/icons";
+import type { ListItemType } from "../types/ComponentTypes";
 import { NavList } from "../components/NavList";
+
+const navListContent: Array<ListItemType> = [
+	{
+		title: "Home",
+		icon: <HomeSmile className="w-5" />,
+		isActive: true,
+	},
+	{
+		title: "Products",
+		icon: <Package className="w-5" />,
+		isActive: false,
+	},
+	{
+		title: "Wishlist",
+		icon: <Heart className="w-5" />,
+		isActive: false,
+	},
+	{
+		title: "Orders",
+		icon: <ShoppingBag01 className="w-5" />,
+		isActive: false,
+	},
+	{
+		title: "Account",
+		icon: <UserCircle className="w-5" />,
+		isActive: false,
+	},
+];
 
 export function SideBar() {
 	return (
 		<aside className="flex flex-col h-full min-w-2xs p-4 gap-4 border-r border-r-gray-200">
 			<div className="flex items-center gap-2 py-4">
-				{/* <div className="w-10">
-					<img src="/public/favicon.svg" alt="Ferio icon" />
-				</div> */}
 				<h1 className="text-text-main font-bold text-4xl">Ferio</h1>
 			</div>
 
-			<NavList />
+			<NavList>
+				{navListContent.map((item) => (
+					<NavList.ListItem {...item} key={item.title} />
+				))}
+			</NavList>
 		</aside>
 	);
 }
