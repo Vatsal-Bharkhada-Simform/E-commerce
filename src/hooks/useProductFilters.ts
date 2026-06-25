0804ac30520e6ea291bd26a93fcf32a702e82985
Vatsal Bharkhada <vatsal.bharkhada@simformsolutions.com>
2026-06-25 12:54:49 +0530

    Refactor: Memoise methods of useProductFilters hook

--- src/hooks/useProductFilters.ts
+++ src/hooks/useProductFilters.ts
@@ -6,6 +6,7 @@ import {
 	validSortPaths,
 } from "../utils/sidebarData";
 import type { ProductListState } from "../types/productTypes";
+import { useCallback } from "react";
 
 type SortPathType = (typeof validSortPaths)[number];
 type PricePathType = (typeof validPricePaths)[number];
@@ -16,63 +17,78 @@ export function useProductParams() {
 	const sortParam = searchParams.get("sort") as SortPathType;
 	const priceParam = searchParams.get("price") as PricePathType;
 
-	function setSortParam(sortPathString: SortPathType) {
-		const updatedParams = new URLSearchParams(searchParams);
-		if (sortPathString === "relevance") {
-			updatedParams.delete("sort");
-		} else {
-			updatedParams.set("sort", sortPathString);
-		}
-		setSearchParams(updatedParams);
-	}
+	const setSortParam = useCallback(
+		function setSortParam(sortPathString: SortPathType) {
+			setSearchParams((prevParams) => {
+				const updatedParams = new URLSearchParams(prevParams);
+				if (sortPathString === "relevance") {
+					updatedParams.delete("sort");
+				} else {
+					updatedParams.set("sort", sortPathString);
+				}
+				return updatedParams;
+			});
+		},
+		[setSearchParams]
+	);
 
-	function setPriceParam(pricePathString: PricePathType) {
-		const updatedParams = new URLSearchParams(searchParams);
-		if (pricePathString === "any") {
-			updatedParams.delete("price");
-		} else {
-			updatedParams.set("price", pricePathString);
-		}
-		setSearchParams(updatedParams);
-	}
+	const setPriceParam = useCallback(
+		function setPriceParam(pricePathString: PricePathType) {
+			setSearchParams((prevParams) => {
+				const updatedParams = new URLSearchParams(prevParams);
+				if (pricePathString === "any") {
+					updatedParams.delete("price");
+				} else {
+					updatedParams.set("price", pricePathString);
+				}
+				return updatedParams;
+			});
+		},
+		[setSearchParams]
+	);
 
-	function sortAndFilterProducts(products: ProductListState) {
-		let productsToDisplay: ProductListState = products;
+	const sortAndFilterProducts = useCallback(
+		function sortAndFilterProducts(products: ProductListState) {
+			let productsToDisplay: ProductListState = products;
 
-		if (sortParam) {
-			if (sortParam === sortOptions.LOW_TO_HIGH.pathString) {
-				productsToDisplay = productsToDisplay.toSorted(
-					(a, b) => a.price - b.price
-				);
-			} else if (sortParam === sortOptions.HIGH_TO_LOW.pathString) {
-				productsToDisplay = productsToDisplay.toSorted(
-					(a, b) => b.price - a.price
-				);
-			}
-		}
-
-		if (
-			priceParam &&
-			validPricePaths.includes(priceParam as PricePathType)
-		) {
-			const selectedPriceOption = Object.values(priceOptions).find(
-				(option) => option.pathString === priceParam
-			);
-			if (selectedPriceOption) {
-				productsToDisplay = productsToDisplay.filter((product) => {
-					const discountedPrice =
-						product.price -
-						(product.price * product.discount) / 100;
-
-					return (
-						discountedPrice > selectedPriceOption.lowerPriceLimit &&
-						discountedPrice <= selectedPriceOption.upperPriceLimit
+			if (sortParam) {
+				if (sortParam === sortOptions.LOW_TO_HIGH.pathString) {
+					productsToDisplay = productsToDisplay.toSorted(
+						(a, b) => a.price - b.price
 					);
-				});
+				} else if (sortParam === sortOptions.HIGH_TO_LOW.pathString) {
+					productsToDisplay = productsToDisplay.toSorted(
+						(a, b) => b.price - a.price
+					);
+				}
 			}
-		}
-		return productsToDisplay;
-	}
+
+			if (
+				priceParam &&
+				validPricePaths.includes(priceParam as PricePathType)
+			) {
+				const selectedPriceOption = Object.values(priceOptions).find(
+					(option) => option.pathString === priceParam
+				);
+				if (selectedPriceOption) {
+					productsToDisplay = productsToDisplay.filter((product) => {
+						const discountedPrice =
+							product.price -
+							(product.price * product.discount) / 100;
+
+						return (
+							discountedPrice >
+								selectedPriceOption.lowerPriceLimit &&
+							discountedPrice <=
+								selectedPriceOption.upperPriceLimit
+						);
+					});
+				}
+			}
+			return productsToDisplay;
+		},
+		[sortParam, priceParam]
+	);
 
 	return {
 		sortParam,
