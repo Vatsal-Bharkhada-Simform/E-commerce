a46ee49c452b02d435d1a9d3a6ec35942990efb4
Vatsal Bharkhada <vatsal.bharkhada@simformsolutions.com>
2026-06-26 18:11:55 +0530

    Update: Remove isLoading flag as isPending covers all use cases. Show total products instead of magic number

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -7,7 +7,6 @@ export function Products() {
 	const { sortAndFilterProducts } = useProductFilters();
 	const {
 		data: products,
-		isLoading,
 		error,
 		isPending,
 	} = useQuery({
@@ -20,7 +19,7 @@ export function Products() {
 		throw new Error("Failed to fetch product data: " + error.message);
 	}
 
-	if (isLoading || isPending) {
+	if (isPending) {
 		return (
 			<div className="col-span-4 flex p-20 justify-center items-center text-xl bg-accent/10 text-accent rounded-4xl mt-8">
 				Loading Products...
@@ -34,7 +33,7 @@ export function Products() {
 		<>
 			<div className="pt-8 pb-4 px-2 text-lg text-gray-600">
 				{productsToDisplay.length > 0 &&
-					`Showing ${productsToDisplay.length} out of ${Math.max(productsToDisplay.length, 200)} products`}
+					`Showing ${productsToDisplay.length} out of ${products.length} products`}
 			</div>
 			<div className="grid sm:grid-cols-3 md:grid-cols-4 gap-8">
 				{productsToDisplay.length === 0 ? (
