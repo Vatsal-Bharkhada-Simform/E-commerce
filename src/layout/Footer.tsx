052ea7a985362dcf0dfff29e2ed0933244b15d09
Vatsal Bharkhada <vatsal.bharkhada@simformsolutions.com>
2026-06-26 11:54:28 +0530

    Update: Hide focus on search button on pages other than products page

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,15 +1,22 @@
+import { useLocation } from "react-router";
+import { ROUTES } from "../routes/routeStrings";
+
 export function Footer({ focusOnSearch }: { focusOnSearch: () => void }) {
+	const { pathname } = useLocation();
+
 	return (
 		<footer className="p-2 border-t border-t-gray-200 flex justify-center gap-4 text-sm">
 			<span className="text-gray-500">
 				&#169; 2026 Ferio, Built by Vatsal Bharkhada
 			</span>
-			<button
-				className="hover:underline cursor-pointer"
-				onClick={focusOnSearch}
-			>
-				Focus on search
-			</button>
+			{pathname === ROUTES.PRODUCT.ROOT && (
+				<button
+					className="hover:underline cursor-pointer"
+					onClick={focusOnSearch}
+				>
+					Focus on search
+				</button>
+			)}
 		</footer>
 	);
 }
